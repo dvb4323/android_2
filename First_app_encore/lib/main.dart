bfa44631f6b6abad05b400c266c8a68e3fe8616d
Do Van Binh <111190439+dvb4323@users.noreply.github.com>
2024-05-19 11:42:03 +0700

    Try an approach to change app bar's color to black

--- First_app_encore/lib/main.dart
+++ First_app_encore/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
