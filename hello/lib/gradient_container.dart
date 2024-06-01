16539a684cec5d6e2ad0a8e26851a2cd38026469
Do Van Binh <111190439+dvb4323@users.noreply.github.com>
2024-06-01 08:18:18 +0700

    Push Source Code for DiceRoller

---
+++ hello/lib/gradient_container.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:hello/styled_text.dart';
+import 'package:hello/dice_roller.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colors, {super.key});
+
+  final List<Color> colors;
+
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
